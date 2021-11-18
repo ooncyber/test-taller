be76c5cdf51844385142bb77bfe6a1db8ec5c3dc
Gabriel Carneiro <Gabriel.Carneiro@targetsistemas.com.br>
2021-11-18 19:05:47 -0300

    add comment to log

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-var list = [4, 8, 0, 9, 2, 5, 0, 3, 3, 0];
+// This is the first array presented
+var list = [4, 8, 0, 9, 2, 5, 0, 3, 3, 0]
+
+// This, is the last array, used for example the sum
+// var list = [4, 8, 9, 2, 5, 8, 4, 9];
 
 
 // move all 0's to right
@@ -9,9 +13,15 @@ list.sort((firstNumber, secondNumber) =>
 // remove 0's from array
 list = list.filter(number => number != 0)
 
+// Log if you want to seee the list without 0's
+// console.log(`list: `, list)
+
 // transform list to array and increment +1 at the value
 let number = parseInt(list.join(''));
 number++;
+
+// Log if you want to be the number
+// console.log(`number: `, number)
 
 // convert the number value to array again and turn all them into number
 list = number.toString().split('').map(e => parseInt(e))
@@ -20,6 +30,9 @@ list = number.toString().split('').map(e => parseInt(e))
 list = list.map(function (number, index) {
     return index % 2 == 0 ? number * -1 : number;
 })
+
+// Log if you want to be the list with negative numbers
+// console.log(`list: `, list)
 
 // define max to min value possible to find out what the higher value
 var max = Number.MIN_VALUE;
